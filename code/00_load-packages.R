374af2608c7bf95519fe6e529c180b0c93812199
Chris Beltz <cwbeltz@aurora.nceas.ucsb.edu>
2020-11-30 09:36:59 -0800

    initial commit for folder structure and R packages

---
+++ code/00_load-packages.R
@@ -0,0 +1,19 @@
+#---
+#  title: "FAIR Scores: Creating a Shiny Dashboard"
+#  author: "Christopher W. Beltz"
+#  date created: "2020-11-30"
+#  R version: "4.0.2"
+#  input: "NA"
+#  output: "NA"
+
+#---
+
+
+#load packages
+library(tidyverse)
+library(reshape2)
+library(dataone)
+library(EML)
+library(emld)
+library(patchwork)
+library(here)
