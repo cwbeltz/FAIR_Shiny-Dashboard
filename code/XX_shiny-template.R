614ac5a79b7a23bf00227c09fbe4ef1a884ed4a4
Chris Beltz <christopher.beltz@gmail.com>
2020-11-30 14:49:31 -0800

    Shiny template from RStudio/Grolemund tutorial

--- code/XX_shiny-template.R
+++ code/XX_shiny-template.R
@@ -10,10 +10,17 @@
 
 
 
-ui <- fluidPage()
+ui <- fluidPage(sliderInput(inputId = "num", 
+                            label = "Choose a number", 
+                            value = 25, min = 1, max = 100),
+                plotOutput("hist"))
 
 
-server <- function(input, output){}
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+  })
+}
 
 
 shinyApp(ui = ui, server = server)
