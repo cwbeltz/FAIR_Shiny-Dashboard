92ea12553ec36a904f6331359e9fac0d25f74585
Chris Beltz <christopher.beltz@gmail.com>
2020-12-01 09:21:25 -0800

    basic figure complete.
    TO DO: convert to format for Shiny to set timeframe

---
+++ code/03_calculations.R
@@ -0,0 +1,19 @@
+#---
+#  title: "FAIR Scores: Creating a Shiny Dashboard"
+#  author: "Christopher W. Beltz"
+#  date created: "2020-12-01"
+#  R version: "4.0.2"
+#  input: "NA"
+#  output: "NA"
+
+#---
+
+
+
+#find date of most recent upload
+most_recent_upload <- data.frame(matrix(ncol = 0, nrow = 1))
+most_recent_upload$date <- max(aggChecks_clean$dateUploaded)
+most_recent_upload$pid <- aggChecks_clean$pid[which(aggChecks_clean$dateUploaded==most_recent_upload$date)]
+  
+
+
---
+++ code/04_test-figure.R
@@ -0,0 +1,27 @@
+#---
+#  title: "FAIR Scores: Creating a Shiny Dashboard"
+#  author: "Christopher W. Beltz"
+#  date created: "2020-12-01"
+#  R version: "4.0.2"
+#  input: "NA"
+#  output: "NA"
+
+#---
+
+
+
+#subset data to only those sequenceIds updated within the last week
+sequenceId_over_timeperiod <- aggChecks_clean %>% 
+  filter(dateUploaded > lubridate::floor_date(Sys.time(), "day") - lubridate::days(7)) %>% 
+  summarize(sequenceId = unique(sequenceId))
+
+aggChecks_subset <- aggChecks_clean[aggChecks_clean$sequenceId %in% sequenceId_over_timeperiod$sequenceId,]
+
+
+
+#graph overall scores on y-axis and sequenceIds on the x-axis, with the score of each pid represented by a point
+aggChecks_subset %>% 
+  ggplot(aes(x=sequenceId, y=scoreOverall)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
