1ac2fad4f8f28e38dfebddf7f751b21af4f66420
Chris Beltz <christopher.beltz@gmail.com>
2020-12-01 09:34:37 -0800

    Shiny displays correct slides and figure.
    TO DO: Get input from slider and figure to interact.

--- code/app.R
+++ code/app.R
@@ -8,17 +8,29 @@
 
 #---
 
-library(shiny)
+library(here)
 
-ui <- fluidPage(sliderInput(inputId = "num", 
-                            label = "Choose a number", 
-                            value = 25, min = 1, max = 100),
-                plotOutput("hist"))
+source(here("code", "00_load-packages.R"))
+source(here("code", "01_load-data.R"))
+source(here("code", "02_clean-data.R"))
+source(here("code", "03_calculations.R"))
+source(here("code", "04_test-figure.R"))
+
+
+
+ui <- fluidPage(sliderInput(inputId = "timeframe", 
+                            label = "Choose a number of days", 
+                            value = 7, min = 1, max = 31),
+                plotOutput("scatterplot"))
+
 
 
 server <- function(input, output) {
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
+  output$scatterplot <- renderPlot({
+    aggChecks_subset %>% 
+      ggplot(aes(x=sequenceId, y=scoreOverall)) +
+      geom_point() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
   })
 }
 
