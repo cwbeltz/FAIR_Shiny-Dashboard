54fa30f85b050dbc7badbf9c9f6e61a9ac2ce562
Chris Beltz <christopher.beltz@gmail.com>
2020-12-02 08:31:15 -0800

    additional comments added, specific packages specified for all functions, and unnecessary code removed.

--- app.R
+++ app.R
@@ -10,14 +10,13 @@
 
 library(here)
 
-source(here("code", "00_load-packages.R"))
-source(here("code", "01_load-data.R"))
-source(here("code", "02_clean-data.R"))
-source(here("code", "03_calculations.R"))
-# source(here("code", "04_test-figure.R"))
+source(here::here("code", "00_load-packages.R"))
+source(here::here("code", "01_load-data.R"))
+source(here::here("code", "02_clean-data.R"))
+source(here::here("code", "03_calculations.R"))
 
 
-
+############ UI block ############ 
 ui <- fluidPage(sliderInput(inputId = "timeframe", 
                             label = "Choose a number of days", 
                             value = 7, min = 1, max = 31),
@@ -26,21 +25,26 @@ ui <- fluidPage(sliderInput(inputId = "timeframe",
 
 
 
+############ Server block ############ 
 server <- function(input, output) {
   
   output$most_recent_date <- renderText(paste0("most recent data uploaded on ", as.Date(most_recent_upload$date)))
   
+  #calculate date to subset dataframe based on user input on days before today
   user_defined_date <- reactive({lubridate::floor_date(Sys.time(), "day") - lubridate::days(input$timeframe)})
   
   
+  
+  #create plot for the FAIR score for each version of a sequenceID
   output$scatterplot <- renderPlot({
+    
+    #obtain sequenceIds for any updated within from user-specified timeframe
     sequenceId_over_timeperiod <- aggChecks_clean %>% 
-      filter(dateUploaded > user_defined_date()) %>% 
-      summarize(sequenceId = unique(sequenceId))
+      dplyr::filter(dateUploaded > user_defined_date()) %>% 
+      dplyr::summarize(sequenceId = unique(sequenceId))
     
+    #subset dataframe using list of sequenceIds
     aggChecks_subset <- aggChecks_clean[aggChecks_clean$sequenceId %in% sequenceId_over_timeperiod$sequenceId,]
-    
-    
     
     #graph overall scores on y-axis and sequenceIds on the x-axis, with the score of each pid represented by a point
     aggChecks_subset %>% 
@@ -51,5 +55,7 @@ server <- function(input, output) {
 }
 
 
+
+############ shinyApp block ############ 
 shinyApp(ui = ui, server = server)
 
