e44c02558013b4610a491460b7517fb1cc8d5c88
Chris Beltz <christopher.beltz@gmail.com>
2020-12-04 15:25:40 -0500

    x-axis tick labels removed from data package FAIR score figure.
    Published and running on shinyapp.io.

--- app.R
+++ app.R
@@ -94,7 +94,10 @@ server <- function(input, output) {
       scale_size_manual(values=sizeValues,
                          name="",
                          labels=c("FINAL", "INITIAL", "INTERMEDIATE")) +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),   #TODO_CWB: Remove at later date once submitter is available.
+              axis.ticks.x=element_blank())
       
       scatter_plot
   })
